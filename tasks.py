4a13104c29191ca6318d2eefce5e8f18a711e508
André Sabino <andre.sabino@gmail.com>
2020-04-05 16:23:50 +0100

    Add tasks to run the program.

--- tasks.py
+++ tasks.py
@@ -8,3 +8,11 @@ def test(c):
 def cov(c):
     c.run("coverage report")
     c.run("coverage html")
+
+@task
+def gui(c):
+    c.run("python program.py gui")
+
+@task
+def cli(c):
+    c.run("python program.py")
