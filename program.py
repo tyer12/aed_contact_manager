44f9613aa80bb1c4876e73039ea9ad9088cf8db1
André Sabino <andre.sabino@gmail.com>
2020-04-05 01:57:28 +0100

    Add gui support to main program.

--- program.py
+++ program.py
@@ -1,4 +1,14 @@
 from views.terminal import Terminal
+from views.gui import ContactManagerGUI
+import sys
 
 if __name__ == "__main__":
-    terminal = Terminal()
+    if len(sys.argv) == 2:
+        if sys.argv[1] == "gui":
+            ContactManagerGUI()
+        elif sys.argv[1] == "terminal":
+            Terminal()
+        else:
+            print("Not supported.")
+    else:
+        Terminal()
---
+++ views/gui.py
@@ -0,0 +1,6 @@
+from controllers import ContactManager
+
+class ContactManagerGUI:
+    def __init__(self):
+        self.cm = ContactManager()
+    
